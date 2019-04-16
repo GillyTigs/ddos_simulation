121206e9166e07a36a65603d2486ec7a1dca3c58
Lucas Monteiro de Oliveira <lucas.o@aluno.ufabc.edu.br>
2019-04-16 02:31:50 -0300

    attacker, dns and firewall initial implementation

--- ddos_sim.py
+++ ddos_sim.py
@@ -3,27 +3,40 @@ import sys
 import getopt
 
 
-help_string="ddos_sim.py -m <syn_flood | dns_amp> -n <qty of botnets> -s <packet size> -p <probability of success>"
+help_string="\n###################\n###DDOS SIM Help###\n###################\n\n#For SYN Flood:\n\n  - ddos_sim.py -m syn_flood -n <qty of botnets> -s <packet size> -p <probability of success>\n\n#For DNS Amplification:\n\n  - ddos_sim.py -m dns_amp -n <qty of botnets> -s <packet size> -a <amplification rate>\n"
 
-#Function to generate a load based on the attacking parameters
-def attacker (method, nBots, pktSize, prob):
-    if method == "syn_flood":
-        print("syn")
-    elif method == "dns_amp":
-        print("dns")
-    
-    print("Chosen method: ", method)
-    print("Quantity of bots: ", nBots)
-    print("Packet size: ", pktSize)
-    print("Probability of success: ", prob)
+#Function to generate the initial load coming from the botnets
+def attacker(n_bots, pkt_size, prev_rand):
+    rand = (random.randint(40,100)/100 + prev_rand)/2 #Insert uncertainty to the bots availability
+    prev_rand = rand
+    return pkt_size * n_bots * rand
+
+
+#Vulnerable DNS for the amplification
+def dns(bot_load, amp_rate):
+    print("Bots -> DNS : ", round(bot_load, 2))
+    return bot_load * amp_rate
+
+
+#Firewall that is going to filter the attack
+def firewall(method, attack_load, defense_clock):
+    print("-> Firewall : ", round(attack_load, 2))
+    '''if method == "dns_amp":
+        defense_clock = defense_clock + (random.randint(0,100)/100)'''
+        
+
+
 
 def main(argv):
     method = ""
     nBots = ""
     pktSize = ""
     prob = ""
+    amp_rate = ""
+    prev_attack_rand = 0
+    defense_clock = 0
     try:
-        opts, args = getopt.getopt(argv,"hm:n:s:p:")
+        opts, args = getopt.getopt(argv,"hm:n:s:pa:")
     except getopt.GetoptError:
         print(help_string)
         sys.exit(2)
@@ -39,22 +52,26 @@ def main(argv):
             elif opt == "-m" and arg in ["syn_flood","dns_amp"]:
                 method = arg
             elif opt == "-n":
-                nBots = arg
+                nBots = float(arg)
             elif opt == "-s":
-                pktSize = arg
-            elif opt == "-p":
+                pktSize = float(arg)
+            elif opt == "-p" and method == "syn_flood":
                 if float(arg)>=0 and float(arg)<=1:
-                    prob = arg
+                    prob = float(arg)
                 else:
                     print("Probability of success (p) should be [0,1]")
                     sys.exit()
+            elif opt == "-a" and method == "dns_amp":
+                amp_rate = float(arg)
             else:
                 print(help_string)
                 sys.exit()
 #       print("opts: ", opts)
 #       print("Chosen method: ", method)
 #       print("Quantity of bots: ", nBots)
-        attacker(method, nBots, pktSize, prob)
+        if method == "dns_amp":
+            for i in range(1,3600):
+                firewall(method,dns(attacker(nBots, pktSize, prev_attack_rand), amp_rate), defense_clock)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
