ba14e21d7ebbfa4210c5ef995cdc8258836546a2
Lucas Monteiro de Oliveira <lucas.o@aluno.ufabc.edu.br>
2019-04-17 19:32:34 -0300

    Matplot added to both attacks

--- ddos_sim.py
+++ ddos_sim.py
@@ -2,6 +2,8 @@ import random
 import sys
 import getopt
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 def printHelp():
     print("\n###################"
@@ -27,42 +29,42 @@ def printHelp():
           " -a <amplification rate>\n\n")
 
 
-#Softened random number creator, prev_rand to soften the variance
+# Softened random number creator, prev_rand to soften the variance
 def newRand(prev_rand, floor):
-    return (random.randint(floor,100)/50 + prev_rand)/3
+    return (random.randint(floor, 100)/50 + prev_rand)/3
 
 
-#Function to generate the initial load coming from the botnets
-#Load is generated in Mbps
+# Function to generate the initial load coming from the botnets
+# Load is generated in Mbps
 def attacker(n_bots, pkt_size, band, rand):
-    #Insert uncertainty to the bots availability
+    # Insert uncertainty to the bots availability
     return round((pkt_size * n_bots * band * rand)/1000000, 2)
 
 
-#Function to generate the connections load
+# Function to generate the connections load
 def attackerCon(n_bots, band, rand):
-    #Insert uncertainty to the bots availability
+    # Insert uncertainty to the bots availability
     return int(n_bots * band * rand)
 
 
-#Function to create a variable legitim traffic volume
+# Function to create a variable legitim traffic volume
 def defaultTraffic(n_bots, pkt_size, band, perc, rand):
     base_traffic = ((pkt_size * n_bots * band)/1000000) * perc
     return round(base_traffic * rand, 2)
 
 
-#Function to create a variable legitim connectio volume
+# Function to create a variable legitim connectio volume
 def defaultCon(n_bots, band, perc, rand):
     return int(n_bots * band * perc * rand)
 
 
-#Vulnerable DNS for the amplification
+# Vulnerable DNS for the amplification
 def dns(bot_load, amp_rate):
-    #print("Bots -> DNS        : ", round(bot_load, 2))
+    # print("Bots -> DNS        : ", round(bot_load, 2))
     return bot_load * amp_rate
 
 
-#Non deterministic clock to successfully mitigate/restart the attack
+# Non deterministic clock to successfully mitigate/restart the attack
 def reactionClock(clock, reaction):
     clock = clock + (random.randint(0,100)/100)
     if clock >= reaction/2:
@@ -71,7 +73,7 @@ def reactionClock(clock, reaction):
         return clock
 
 
-#Check if the server is down and returns why
+# Check if the server is down and returns why
 def isDown(link, attack_load, attack_connections, connections):
     if attack_load > link or attack_connections > connections:
         return 1
@@ -79,7 +81,7 @@ def isDown(link, attack_load, attack_connections, connections):
         return 0
 
 
-#Firewall that is going to filter the attack
+# Firewall that is going to filter the attack
 def firewall(method, attack_load, default_traffic, block_status, link, row, att_con=0, def_con=0, con=0):
     inbound = round(attack_load + default_traffic, 2)
     row.append(inbound)
@@ -90,15 +92,15 @@ def firewall(method, attack_load, default_traffic, block_status, link, row, att_
     multi = abs(block_status - 1)
     target_load = (multi * attack_load) + default_traffic
     row.append(round(target_load, 2))
-    #print("Firewall -> Target : ", round(target_load, 2))
+    # print("Firewall -> Target : ", round(target_load, 2))
     target_con = (multi * att_con) + def_con
     s_down = isDown(link, target_load, target_con, con)
     rem_band = round(link - target_load, 2)
     rem_con = con - target_con
     row.append(rem_band)
-    #print("Remaining bandwidth: ", rem_band)
+    # print("Remaining bandwidth: ", rem_band)
     if method == "syn_flood":
-    #    print("Remaining connections: ", rem_con)
+        # print("Remaining connections: ", rem_con)
         row.append(rem_con)
     '''if s_down == 1:
         print("Server is          :  DOWN!!!")
@@ -106,6 +108,79 @@ def firewall(method, attack_load, default_traffic, block_status, link, row, att_
         print("Server is: UP")
     '''
     row.append(s_down)
+
+def plotGraph(method, df):
+    if method == "dns_amp":
+        plt.close('all')
+
+        plt.figure(1)
+        plt.plot(df['second'], df['rem_bandwidth'],
+                    color='red', label='remaining bandwidth')
+        plt.plot(df['second'], df['bot-->dns'],
+                    color='blue', label='bot-->dns')
+        plt.plot(df['second'], df['dns-->firewall'],
+                    color='yellow', label='dns-->firewall')
+        plt.plot(df['second'], df['firewall-->target'],
+                    color='green', label='firewall-->target')
+        plt.legend(loc='best', bbox_to_anchor=(0.5, 0., 0.5, 0.5))
+        plt.xlabel('Time (s)')
+        plt.ylabel('Bandwidth (Mbps)')
+        plt.title("Bandwidth Status")
+        plt.grid(True)
+        plt.savefig('bandwidth_status.png', bbox_inches='tight')
+
+        plt.figure(2)
+        plt.plot(df['second'], df['server_down'],
+                    color='purple', label='server status')
+        plt.legend(loc='best', bbox_to_anchor=(0.5, 0., 0.5, 0.5))
+        plt.xlabel('Time (s)')
+        plt.ylabel('Up/Down')
+        plt.title("Server Status")
+        plt.grid(True)
+        plt.savefig('server_status.png', bbox_inches='tight')
+
+        plt.show()
+    elif method == "syn_flood":
+        plt.close('all')
+
+        plt.figure(1)
+        plt.plot(df['second'], df['bot-->firewall'],
+                    color='red', label='bot-->firewall')
+        plt.plot(df['second'], df['firewall-->target'],
+                    color='green', label='firewall-->target')
+        plt.plot(df['second'], df['rem_bandwidth'],
+                    color='blue', label='rem_bandwidth')
+        plt.legend(loc='best', bbox_to_anchor=(0.5, 0., 0.5, 0.5))
+        plt.xlabel('Time (s)')
+        plt.ylabel('Bandwidth (Mbps)')
+        plt.title("Bandwidth Status")
+        plt.grid(True)
+        plt.savefig('bandwidth_status.png', bbox_inches='tight')
+
+        plt.figure(2)
+        plt.plot(df['second'], df['server_down'],
+                    color='purple', label='server status')
+        plt.legend(loc='best', bbox_to_anchor=(0.5, 0., 0.5, 0.5))
+        plt.xlabel('Time (s)')
+        plt.ylabel('Up/Down')
+        plt.title("Server Status")
+        plt.grid(True)
+        plt.savefig('server_status.png', bbox_inches='tight')
+
+        plt.figure(3)
+        plt.plot(df['second'], df['rem_connections'],
+                    color='blue', label='remaining connections')
+        plt.legend(loc='best', bbox_to_anchor=(0.5, 0., 0.5, 0.5))
+        plt.xlabel('Time (s)')
+        plt.ylabel('Connections')
+        plt.title("Remaining Connections")
+        plt.grid(True)
+        plt.savefig('connections.png', bbox_inches='tight')
+
+        plt.show()
+        pass
+    else:
+        pass
 
 
 def main(argv):
@@ -126,8 +201,9 @@ def main(argv):
     attack_clock = 0
     block_status = 0
     server_down = 0
+
     try:
-        opts, args = getopt.getopt(argv,"hm:n:s:b:r:p:l:a:c:")
+        opts, args = getopt.getopt(argv, "hm:n:s:b:r:p:l:a:c:")
     except getopt.GetoptError:
         printHelp()
         sys.exit(2)
@@ -140,7 +216,7 @@ def main(argv):
             if opt == "-h" or "-" in arg:
                 printHelp()
                 sys.exit()
-            elif opt == "-m" and arg in ["syn_flood","dns_amp"]:
+            elif opt == "-m" and arg in ["syn_flood", "dns_amp"]:
                 method = arg
             elif opt == "-n":
                 n_bots = float(arg)
@@ -163,18 +239,24 @@ def main(argv):
                 sys.exit()
 
         if method == "dns_amp":
-            df = pd.DataFrame(columns=["second", "bot-->dns", "dns-->firewall",
-                                       "firewall-->target", "rem_bandwidth",
+            df = pd.DataFrame(columns=["second",
+                                       "bot-->dns",
+                                       "dns-->firewall",
+                                       "firewall-->target",
+                                       "rem_bandwidth",
                                        "server_down"])
         elif method == "syn_flood":
-            df = pd.DataFrame(columns=["second", "bot-->firewall",
-                                       "firewall-->target", "rem_bandwidth",
-                                       "rem_connections", "server_down"])
+            df = pd.DataFrame(columns=["second",
+                                       "bot-->firewall",
+                                       "firewall-->target",
+                                       "rem_bandwidth",
+                                       "rem_connections",
+                                       "server_down"])
 
         for i in range (1,period+1):
-            #print("\n\n####################")
-            #print("Second: ", i)
-            #print("####################")
+            # print("\n\n####################")
+            # print("Second: ", i)
+            # print("####################")
             if block_status == 0:
                 defense_clock = reactionClock(defense_clock, reaction)
                 if defense_clock == 0:
@@ -183,9 +265,9 @@ def main(argv):
                 attack_clock = reactionClock(attack_clock, reaction)
                 if attack_clock == 0:
                     block_status = 0
-            prev_attack_rand = newRand(prev_attack_rand,75)
-            prev_traffic_rand = newRand(prev_traffic_rand,50)
-            prev_con_rand = newRand(prev_con_rand,50)
+            prev_attack_rand = newRand(prev_attack_rand, 75)
+            prev_traffic_rand = newRand(prev_traffic_rand, 50)
+            prev_con_rand = newRand(prev_con_rand, 50)
             if method == "dns_amp":
                 row = [i]
                 bot_load = attacker(n_bots, pkt_size, band, prev_attack_rand)
@@ -201,11 +283,12 @@ def main(argv):
                 att_con = attackerCon(n_bots, band, prev_attack_rand)
                 firewall(method, att_load, def_traffic, block_status, link, row, att_con, def_con, connections)
 
-            #print("LINHA:           ", row)
+            # print("LINHA:           ", row)
             df.loc[len(df)] = row
 
         print(df)
+        plotGraph(method, df)
 
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
